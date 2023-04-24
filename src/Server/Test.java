465a51318a4b85f452678de2e064a25777bee77a
iliav <ilia.vrtn@gmail.com>
2023-04-24 14:59:26 +0300

    First Commit

--- src/Server/Test.java
+++ src/Server/Test.java
@@ -1,10 +1,12 @@
 package Server;
 
 import Client.Boris;
+import Client.Ilya;
 
 public class Test {
 	public static void main(String[] args) {
 		Boris boris = new Boris();
+		Ilya ilya = new Ilya();
 	}
 	
 }
