e320e9b7cde78b7518102c36168ae3d9193b28f6
iliav <ilia.vrtn@gmail.com>
2023-04-24 14:58:37 +0300

    First Commit

--- src/Client/Ilya.java
+++ src/Client/Ilya.java
@@ -1,5 +1,7 @@
 package Client;
 
 public class Ilya {
-
+	public Ilya(){
+        System.out.println("This is Ilya\n");
+    }
 }
--- src/Server/IlyaTest.java
+++
@@ -1,9 +0,0 @@
-package Server;
-
-import Client.Ilya;
-
-public class IlyaTest {
-	public static void main(String[] args) {
-		Ilya ilya = new Ilya();
-	}
-}
