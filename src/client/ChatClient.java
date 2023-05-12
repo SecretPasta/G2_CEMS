06c864195b3d8e730b1d83f71a188f297d6d8ab9
ndvp39 <ndvp39@gmail.com>
2023-05-12 20:03:51 +0300

    added comment

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -2,7 +2,7 @@
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at www.lloseng.com 
 
-package client;
+package client; //test
 
 import ocsf.client.*;
 import client.*;
