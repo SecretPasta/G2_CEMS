a8689db0d3ab8c5e70323242a199f08c902070c4
ndvp39 <ndvp39@gmail.com>
2023-06-10 19:07:19 +0300

    working on extra time for exam

--- src/lecturer/LecturerDashboardFrameController.java
+++ src/lecturer/LecturerDashboardFrameController.java
@@ -39,6 +39,7 @@ import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.util.StringConverter;
@@ -70,11 +71,6 @@ public class LecturerDashboardFrameController implements Initializable{
 	
 	@FXML
 	private JFXSnackbar snackbarError;
-	
-	@FXML
-	private Label lblMessage1;
-	@FXML
-	private Label lblMessage2;
 	@FXML
 	private Label lbluserNameAndID;
 	@FXML
@@ -928,7 +924,36 @@ public class LecturerDashboardFrameController implements Initializable{
 	
 	
 	public void getBtnExtraTime_ManageExams(ActionEvent event) throws Exception {
+		activeExamSelected = tableView_activeExams.getSelectionModel().getSelectedItem();
 		
+		try {
+	        if (activeExamSelected == null) {
+	            throw new NullPointerException();
+	        }
+	        ((Node) event.getSource()).getScene().getWindow().hide(); // Hide the primary window
+	        ManageExam_ChangeTimeFrameController.start(activeExamSelected, lecturer);
+
+	    } catch (NullPointerException e) {
+	        snackbarError = new JFXSnackbar(pnlManageExams);
+	        snackbarError.setPrefWidth(754);
+	        snackbarError.fireEvent(new SnackbarEvent(new JFXSnackbarLayout("[Error] Exam not selected"), Duration.millis(3000), null));
+	    }
+	    
+	    tableView_inActiveExams.getSelectionModel().clearSelection();
+	    tableView_activeExams.getSelectionModel().clearSelection();
+	    tableView_activeExams.refresh();
+	    tableView_inActiveExams.refresh();
+
+	    activeExamSelected = null;
+	}
+	
+	public void showDashboardFrom_ChangeTime() throws IOException {
+	    // Show the current stage
+	    currStage.show();
+
+	    // Clear the selection in the exam tables
+	    tableView_inActiveExams.getSelectionModel().clearSelection();
+	    tableView_activeExams.getSelectionModel().clearSelection();
 	}
 
 	
