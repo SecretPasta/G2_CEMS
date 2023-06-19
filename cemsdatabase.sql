71df35fcb35028d5d20b6fc0f81d07c68e2b81cf
SecretPasta <boris.shoostin@gmail.com>
2023-06-19 20:40:47 +0300

    Added Manual Exam examples to DB

--- cemsdatabase.sql
+++ cemsdatabase.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `cemsdatabase` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `cemsdatabase`;
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: cemsdatabase
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -439,7 +437,7 @@ CREATE TABLE `manualexams` (
 
 LOCK TABLES `manualexams` WRITE;
 /*!40000 ALTER TABLE `manualexams` DISABLE KEYS */;
-INSERT INTO `manualexams` VALUES ('123456','01','02','potato.txt','C:/TestFiles','Manual Exam','I like potatoes',180,'001','Ben Dover','6942042069','1');
+INSERT INTO `manualexams` VALUES ('123456','01','02','Exam1.docx','C:/Exams','Manual Exam','I like potatoes',180,'001','Ben Dover','6942042069','1'),('456789','02','03','Exam.pdf','C:/Exams','Please Complete','Butter Dog',120,'002','Magic Mike','1546846216','1');
 /*!40000 ALTER TABLE `manualexams` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -623,4 +621,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-19 15:33:04
+-- Dump completed on 2023-06-19 20:40:14
